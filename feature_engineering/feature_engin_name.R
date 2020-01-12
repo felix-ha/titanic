cbbe91b283caa03bb50b3e435926273046896785
Felix Hauer <hauerf98@gmail.com>
2020-01-12 19:03:28 +0100

    added bootstrap ci to plot

--- feature_engineering/feature_engin_name.R
+++ feature_engineering/feature_engin_name.R
@@ -6,6 +6,8 @@ rm(list = ls())
 library(stringr)
 library(tidyverse)
 library(magrittr)
+library(boot)
+
 
 df <- read_csv("train.csv")
 df_test <- read_csv("test.csv")
@@ -65,12 +67,12 @@ ggplot(df_title)  +
   theme_bw()
 
 
-stop("plot / calculate survival probabilties here")
-ggplot(df_title)  +
-  geom_bar(aes(x = TitleGroup, fill = Survived),
-           position=position_dodge()) +
-  theme_bw()
+
   
+
+
+# Statistik Test ----------------------------------------------------------
+
 
 
 table(df_title$TitleGroup)
@@ -78,10 +80,68 @@ table_abs <- table(df_title$TitleGroup, df_title$Survived)
 
 prop.table(table_abs, 1)
 
-prop.test(table_abs)
+test <- prop.test(table_abs)
+print(test)
+
+
 
 fit <- glm(Survived ~ TitleGroup, data=df_title, family =binomial(link = "logit"))
 summary(fit)
+
+
+
+
+# Plot with custom bootstrap ci  ------------------------------------------
+
+
+boot_mean <- function(values,i) mean(values[i])
+alpha <- 0.05
+
+number_of_factors <- length(levels(df_title$TitleGroup))
+ymin <- vector(mode = "numeric", length = number_of_factors)
+ymax <- vector(mode = "numeric", length = number_of_factors)
+
+for(i in 1:number_of_factors) {
+  
+  factor_current <- levels(df_title$TitleGroup)[i]
+  
+  values <- df_title %>% 
+    filter(TitleGroup == factor_current) %>% 
+    select(Survived) %>% 
+    mutate(as.integer(Survived) - 1) %>%
+    pull()
+  
+  fit <- boot(values, boot_mean, R = 100)
+  quantiles <- quantile(fit$t,c(alpha / 2, 1 - (alpha / 2)))
+  
+  ymin[i] <- quantiles[1]
+  ymax[i] <- quantiles[2]
+}
+
+
+df_plot <- df_title %>%
+  group_by(TitleGroup) %>%
+  summarise(count = n(),
+            prob = mean(as.numeric(Survived) - 1))   %>%
+  mutate(TitleGroup = fct_reorder(TitleGroup, prob, .desc = TRUE))
+
+
+ggplot(df_plot, aes(x = TitleGroup, y = prob)) + 
+  geom_col(width = 0.75) + 
+  geom_errorbar(ymin = ymin,
+                ymax = ymax,
+                width = .75) +
+  theme_bw() + 
+  labs(
+    #x = "Models",
+    #y = "AUC",
+    title = paste0("Survival Probabilities of factor TitleGroup with ", 1 - alpha, " CI"),
+    subtitle = paste0(" p value of Test \"H0: probabilities are the same\": ", signif (test$p.value, 5))
+  ) +
+  ylim(c(0,1))
+
+
+
 
 
 # Summary Processing
